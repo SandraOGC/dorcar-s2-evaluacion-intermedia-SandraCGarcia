e80e2d518d6db5987b96b240f5ddeacbd454cb4a
SandraCGarcia <sandra.gcanete@gmail.com>
2018-07-15 21:06:01 +0200

    sin cambios

--- main.js
+++ main.js
@@ -1,73 +1,57 @@
-'use strict';
+"use strict";
 
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-  }
+  return Math.ceil(Math.random() * max);
+}
 //   console.log('> ' + getRandomNumber(100));
 
-var counterElement= document.querySelector(".contador");
-var userNumber= document.querySelector(".number");
-var prueba= document.querySelector(".boton");
-var pista= document.querySelector(".text");
-var randomNumber= getRandomNumber(100);
+var counterElement = document.querySelector(".contador");
+var userNumber = document.querySelector(".number");
+var prueba = document.querySelector(".boton");
+var pista = document.querySelector(".text");
+var randomNumber = getRandomNumber(100);
 
-console.log (randomNumber);
+console.log(randomNumber);
 
-var counter= 0;
+var counter = 0;
+var counterElement = document.querySelector('.contador');
 
-counterElement.innerHTML= counter;
-
-function juego(){
-    var yourNumber=parseInt(userNumber.value);
-    // console.log(yourNumber);
-    if (yourNumber===randomNumber){
-        pista.innerHTML= "Ganador";
-        //ganador
-    }else if (yourNumber<randomNumber){
-        pista.innerHTML= "Te has quedado corto";
-        userNumber.value= "";
-        counter= counter + 1;
-        counterElement.innerHTML= counter;
-
-        //corto
-    }else{
-        pista.innerHTML= "Te has pasado";
-        userNumber.value= "";
-        counter= counter + 1;
-        counterElement.innerHTML= counter;
-        
-    }
+function actualizaContador() {
+  counter = counter + 1;
+  counterElement.innerHTML = counter;
 }
 
+actualizaContador();
+
+counterElement.innerHTML = counter;
+
+function clue (texto){
+    pista.innerHTML = texto;
+  }
+  
+
+function juego() {
+  var yourNumber = parseInt(userNumber.value);
+  // console.log(yourNumber);
+  if (yourNumber === randomNumber) {
+    clue("Ganador");
+    //ganador
+  } else if (yourNumber < randomNumber) {
+    clue("Te has quedado corto");
+    userNumber.value = "";
+    // counter = counter + 1;
+    actualizaContador();
+    // counterElement.innerHTML = counter;
+
+    //corto
+  } else {
+    clue ("Te has pasado");
+    userNumber.value = "";
+    // counter = counter + 1;
+    actualizaContador();
+    // counterElement.innerHTML = counter;
+  }
+}
 
 prueba.addEventListener("click", juego);
 
-
-
-
-
-
-
-
-//   var random= getRandomNumber;
-
-// var chooseNumber= document.querySelector('.number');
-// var button = document.querySelector('.boton');
-
-// function play(){
-//     console.log(chooseNumber.value);
-//     if (chooseNumber = getRandomNumber) {
-//         console.log = 'Bien'; 
-//     }
-// }
-// button.addEventListener('click', play);
-
-
-// // function contador(){
-// //     for (var i=0; i++);
-
-// //     }
-
-//   var miAleatorio= (getRandomNumber(100));
-
-//   varContador.innerHTML
